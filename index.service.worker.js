2cbd74a5b1ac0e8aa4d50a504fd1a8a8217d6453
Ben Blum <benblum@macbookpro.local>
2026-09-10 10:05:44 +0300

    Deploy blocky animations and icons (source 7aefced)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789022308|1967624';
+const CACHE_VERSION = '1789023900|1909115';
 /** @type {string} */
 const CACHE_PREFIX = 'Diablo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
