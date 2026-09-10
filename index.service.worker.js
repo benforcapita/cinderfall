5e8978ab758cea8904f8a8ef088c697ded3886e7
Ben Blum <benblum@macbookpro.local>
2026-09-10 14:23:48 +0300

    Deploy inventory scrolling and slot display (source 30fc758)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789035445|1797613';
+const CACHE_VERSION = '1789039322|1697617';
 /** @type {string} */
 const CACHE_PREFIX = 'Diablo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
